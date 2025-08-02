79a74fa575fbe50be7eb761bdedd4f7bd0eb7219
unknown <ahmedtahoor5@gmail.com>
2025-08-02 05:21:59 +0500

    done

--- src/components/fliter-header.tsx
+++ src/components/fliter-header.tsx
@@ -1,23 +1,30 @@
 "use client";
 
 import { ChevronDown } from "lucide-react";
+import Link from "next/link";
 
 export default function NavigationButtons() {
   return (
     <div className="flex justify-center items-center gap-2 pt-2 pb-4 container">
-      <button className="bg-primary hover:bg-primary text-black font-semibold px-3 py-1.5 text-[16px] rounded-md flex items-center gap-2 transition-colors duration-200">
+      <Link
+        href={"/retailers"}
+        className="bg-primary hover:bg-primary text-black font-semibold px-3 py-1.5 text-[16px] rounded-md flex items-center gap-2 transition-colors duration-200"
+      >
         RETAILERS
-        <ChevronDown className="w-4 h-4" />
-      </button>
+        {/* <ChevronDown className="w-4 h-4" /> */}
+      </Link>
 
-      <button className="bg-primary hover:bg-primary text-black font-semibold px-3 py-1.5 text-[16px] rounded-md flex items-center gap-2 transition-colors duration-200">
+      <Link
+        href={"/category"}
+        className="bg-primary hover:bg-primary text-black font-semibold px-3 py-1.5 text-[16px] rounded-md flex items-center gap-2 transition-colors duration-200"
+      >
         CATEGORIES
-        <ChevronDown className="w-4 h-4" />
-      </button>
+        {/* <ChevronDown className="w-4 h-4" /> */}
+      </Link>
 
       <button className="bg-primary hover:bg-primary text-black font-semibold px-3 py-1.5 text-[16px] rounded-md flex items-center gap-2 transition-colors duration-200">
         COUPONS & DEALS
-        <ChevronDown className="w-4 h-4" />
+        {/* <ChevronDown className="w-4 h-4" /> */}
       </button>
     </div>
   );
--- src/components/shop-category-item.tsx
+++ src/components/shop-category-item.tsx
@@ -1,5 +1,6 @@
 import { category } from "@/assets";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   name: string;
@@ -8,7 +9,10 @@ interface CategoryItemProps {
 
 export function CategoryItem({ name, image }: CategoryItemProps) {
   return (
-    <div className="flex flex-col items-center group cursor-pointer">
+    <Link
+      href={"/offer"}
+      className="flex flex-col items-center group cursor-pointer"
+    >
       <div className="relative w-16 h-16 sm:w-24 sm:h-24 mb-4 transition-transform duration-200 group-hover:scale-105">
         <Image
           src={category}
@@ -22,6 +26,6 @@ export function CategoryItem({ name, image }: CategoryItemProps) {
       <span className="text-lg font-semibold text-black text-center leading-tight group-hover:text-gray-900 transition-colors duration-200">
         {name}
       </span>
-    </div>
+    </Link>
   );
 }
