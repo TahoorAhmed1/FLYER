0be357bcc87c519fc2aca0ac1c2942407d39b722
unknown <ahmedtahoor5@gmail.com>
2025-08-02 05:24:07 +0500

    done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -55,7 +55,7 @@ function Header() {
             href={"/wishlist"}
             className=" bg-primary hover:bg-primary/70 px-3 py-2 text-black font-semibold rounded-[10px] text-[18px]"
           >
-            Join Waitlist
+            Join Wishlist
           </Link>
         </div>
       </div>
